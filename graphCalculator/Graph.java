90f1a1866b68407bcf2bc1bd7aa695806d636e14
Tulio <tuliojr3101@gmail.com>
2025-02-17 21:41:30 -0500

    Lunes P00B

--- graphCalculator/Graph.java
+++ graphCalculator/Graph.java
@@ -4,27 +4,33 @@ import java.util.Queue;
 import java.util.LinkedList;
 
 public class Graph {
-    private ArrayList<String> vertexGraph;
-    private ArrayList<ArrayList<String>> aristas;
-    private ArrayList <String> pathing = new ArrayList<>();
+    //private ArrayList<String> vertexGraph;
+    //private ArrayList<ArrayList<String>> aristas;
+    //private ArrayList <String> pathing = new ArrayList<>();
     //no duplocados inicio fin
     //vector de parejas 
     //arcos de vertices que existen
+    private String[] vertex1;
+    private String[][] aristas;
+    private ArrayList<Integer>[] adjList;
     
     public Graph(String [] vertices, String[][] edges){
+        /*
         vertexGraph = new ArrayList<>(Arrays.asList(vertices));
         aristas = new ArrayList<>();
         for (String [] edge : edges){
             aristas.add(new ArrayList<>(Arrays.asList(edge)));
         }
-    
+        */
+       vertex1 = vertices;
+       aristas = edges;    
     }
     /**
      * Determine if a vertex is in the graph
        */
     public boolean contains(String vertex){
-        for (int i = 0; i < vertexGraph.size(); i++){
-            if (vertexGraph.get(i) == vertex){
+        for (int i = 0; i < vertex1.length; i++){
+            if (vertex1[i] == vertex){
                 return true;
             }
         }
@@ -35,29 +41,7 @@ public class Graph {
        */
     
     public Graph path(String start, String end){
-        // Aplicamos BFS
-        int pos = 0;
-        Queue<String> q = new LinkedList();
-        boolean[] visited = new boolean[aristas.size()];
-        for (int i = 0; i < vertexGraph.size(); i++){
-            if (vertexGraph.get(i) == start){
-                pos = i;
-            }
-        }
-        visited[pos] = true;
-        q.add(start);
-        // q.poll retorna la cabeza
-        while (!q.isEmpty()){
-            String current = q.poll();
-            pathing.add(current);
-            for (String x : aristas.get(aristas.indexOf(current))){
-                if(!visited[aristas.indexOf(x)]){
-                    visited[aristas.indexOf(x)] = true;
-                    q.add(x);
-                }
-            }
-        }
-        System.out.println(pathing);
+        boolean [] isVisited = new boolean[vertex1.length];
         return null;
     }
 
@@ -69,14 +53,14 @@ public class Graph {
        */
     
     public int vertices(){
-        return vertexGraph.size();
+        return vertex1.length;
     }
     
     /**
      * Calculates the amount of edges in a graph
        */   
     public int edges(){
-        return aristas.get(0).size();
+        return aristas[0].length;
     }    
     
     
@@ -89,19 +73,18 @@ public class Graph {
     }
     
     //Only arcs in space-separated tuples. The vertices are capitalized. The edges must always be in alphabetical order.
-    //For example, (A, B) (C, D) 
+    //For example, (A, B) (C, D)
+    @Override
     public String toString() {
       return "";
     }
-    public void adjacencyMatrix(ArrayList<ArrayList<String>>aristas){
-        for (ArrayList<String> fila : aristas){
-            for (String val : fila){
-                System.out.println(val + " ");
-            }
-        System.out.println();
+    private void initAdjList(){
+        adjList = new ArrayList[vertex1.length];
+        for (int i = 0; i < vertex1.length; i++){
+            adjList[i] = new ArrayList<>();
         }
     }
-    private void addEdge(ArrayList<ArrayList<String>> aristas, int i, int j){
-        aristas.get(i).set(j, "1");
+    public void addEdge(String a, String b){
+        //adjList[].add(b);
     }
 }
